66c8436a40fabb5a3187d667e5737846b41a11ad
di-legorreta <114834384+di-legorreta@users.noreply.github.com>
2025-09-15 20:49:56 +0200

    Added search functionality with API

--- src/index.js
+++ src/index.js
@@ -1,11 +1,28 @@
 // @ts-check
 
+function refreshWeather(response) {
+   let temperatureElement = document.querySelector("#weather-app-temperature");
+   let temperature = response.data.temperature.current;
+
+   cityElement.innerHTML = response.data.city;
+   temperatureElement.innerHTML = Math.round(temperature);
+}
+
+function searchCity(city) {
+   let apiKey = "297bdob5643aebcfc422bc019b792eta";
+   let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+   console.log(apiUrl);
+   axios.get(apiUrl).then(refreshWeather);
+}
+
 function handleSearchSubmit(event) {
    event.preventDefault();
    let searchInput = document.querySelector("#search-form-input");
-   let cityElement = document.querySelector("#weather-app-city");
-   cityElement.innerHTML = searchInput.value;
+
+   searchCity(searchInput.value);
 }
 
 let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", handleSearchSubmit);
+
+searchCity("Mexico");
